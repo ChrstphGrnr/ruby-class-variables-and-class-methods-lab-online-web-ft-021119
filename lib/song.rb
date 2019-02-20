9b37f705b3143b2365992c32867d95d97e2c8706
Christoph Gruener <christophgruener@me.com>
2019-02-20 20:05:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,17 @@
-class Song 
-attr_accessor :genres :artists :name 
+class Song
+attr_accessor :genres :artists :name
 @@count = 0
 @@genres = []
 @@artists = []
 
   def initialize(name, artist, genre)
-    
+
     @@artists << artist
-    @@genres << genre 
+    @@genres << genre
     @@number_of_songs += 1
   end
 
-  def self.count 
+  def self.count
     @@number_of_songs
   end
 
@@ -19,21 +19,21 @@ attr_accessor :genres :artists :name
     @@genres.uniq
   end
 
-  def self.artists 
+  def self.artists
     @@artists.uniq
   end
 
   def self.genre_count
     genre_hash = {}
     @@genres.uniq.each do |name|
-      genre_hash[name] = 0 
+      genre_hash[name] = 0
     end
     @@genres.each do |name|
-      genre_hash[name] += 1 
+      genre_hash[name] += 1
     end
     genre_hash
   end
 
-  
-  
+
+
 end
